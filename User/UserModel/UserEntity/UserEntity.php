5642b7ff915738e886d59554bda06bdf774f80ed
SwathiAravinthan <swathiaravinth@gmail.com>
2019-01-04 11:40:48 +0530

    User added

---
+++ User/UserModel/UserEntity/UserEntity.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>Untitled Document</title>
+</head>
+
+<body>
+<?php
+$firstname= $_POST['firstname']
+$lastname= $_POST['lastname']
+$email=$_POST[['email_id']
+$gender=$_POST['gender']
+$gender=$_POST['hobbies']
+$gender=$_POST['qualification']
+$gender=$_POST['contact']
+?>
+</body>
+</html>
