20660c2ba1881c57846ba723aad42ae3be36c283
LogeshSanjay <logeshsanjay914@gmail.com>
2019-01-04 11:44:42 +0530

    employee added

---
+++ Employee/UserModel/UserEntity/UserEntity.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>Untitled Document</title>
+</head>
+
+<body>
+
+<?php
+$Empid = $_REQUEST['Empid'];
+$Empname = $_REQUEST['Empname'];
+$Designation= $_REQUEST['Designation'];
+$Address= $_REQUEST['Address'];
+$Ph_no = $_REQUEST['Ph_no'];
+$Email = $_REQUEST['Email'];
+$Salary= $_REQUEST['Salary'];
+?>
+
+
+
+</body>
+</html>
